d122bcd6c7c7359424e68f8b0a0e50113484efd6
Marcos Kloss <marcoskloss2001@gmail.com>
2022-11-09 11:42:47 -0300

    add posts table migration

---
+++ database/migrations/1668004195274_posts.ts
@@ -0,0 +1,22 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'posts'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id', { primaryKey: true })
+      table.text('author').notNullable()
+      table.text('title').notNullable()
+      table.text('content').notNullable()
+      table.text('cover_image_url').notNullable()
+
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
