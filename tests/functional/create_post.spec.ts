103bef443be2a8abddfe28c81bc3ec265032cba5
Marcos Kloss <marcoskloss2001@gmail.com>
2022-11-11 12:04:08 -0300

    refactor to use post route factory

--- tests/functional/create_post.spec.ts
+++ tests/functional/create_post.spec.ts
@@ -1,16 +1,18 @@
 import { test } from '@japa/runner'
 import PostFactory from 'Database/factories/PostFactory'
+import { getPostRoute } from '../testHelpers/routes'
 
 const makePost = async () => {
   const post = await PostFactory.make()
   return post.toJSON()
 }
 
+const postRoute = getPostRoute()
 
 test('should create a new post with success', async (ctx) => {
   const { assert, client } = ctx
   const postPayload = await makePost()
-  const response = await client.post('/posts').json(postPayload)
+  const response = await client.post(postRoute).json(postPayload)
 
   const { id, ...post } = response.body()
 
@@ -23,28 +25,28 @@ test.group('should not create a new post with wrong payload', () => {
   test('should validate a post without author', async ({ client }) => {
     const postPayload = await makePost()
     const { author: _, ...payload } = postPayload
-    const response = await client.post('/posts').json(payload)
+    const response = await client.post(postRoute).json(payload)
     response.assertStatus(422)
   })
 
   test('should validate a post without content', async ({ client }) => {
     const postPayload = await makePost()
     const { content: _, ...payload } = postPayload
-    const response = await client.post('/posts').json(payload)
+    const response = await client.post(postRoute).json(payload)
     response.assertStatus(422)
   })
 
   test('should validate a post without title', async ({ client }) => {
     const postPayload = await makePost()
     const { title: _, ...payload } = postPayload
-    const response = await client.post('/posts').json(payload)
+    const response = await client.post(postRoute).json(payload)
     response.assertStatus(422)
   })
 
   test('should validate a post without cover image url', async ({ client }) => {
     const postPayload = await makePost()
     const { coverImageUrl: _, ...payload } = postPayload
-    const response = await client.post('/posts').json(payload)
+    const response = await client.post(postRoute).json(payload)
     response.assertStatus(422)
   })
 })
